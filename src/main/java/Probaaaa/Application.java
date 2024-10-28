384ac585aa17943cf7edf5eacc7001d2687ad49c
Daniil0Yehorov <danvochdanvoch@gmail.com>
2024-10-28 18:00:26 +0100

    pom dependency update

--- src/main/java/Probaaaa/Application.java
+++ src/main/java/Probaaaa/Application.java
@@ -1,7 +1,6 @@
 package Probaaaa;
 
 
-import Probaaaa.Model.Book;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
